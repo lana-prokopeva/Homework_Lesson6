145a7e1525d34b84c1cf893877e7a4fced3ff82c
Svetlana Prokopeva <lana.prokopeva@yandex.ru>
2022-10-03 20:45:35 +0300

    Task01 added

---
+++ Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Метод определения чисел больше нуля
+int GetCountPositiveNum(int number)
+{
+    int[] NumArray = new int [number];
+    int count = 0;
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        NumArray[i] = int.Parse(Console.ReadLine()); 
+        if (NumArray[i] > 0) count = count + 1;
+    }
+    return count;
+}
+
+// Определяем входные данные
+Console.Write("Укажите количество заданных чисел: ");
+int Num = int.Parse(Console.ReadLine());
+
+int CountPositiveNum = GetCountPositiveNum(Num);
+Console.WriteLine($"Кол-во введенныx положительных чисел = {CountPositiveNum}");
